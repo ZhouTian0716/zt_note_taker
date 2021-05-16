3443fe7a4968d2d46f90533031fce7d96e7a025e
ZhouTian0716 <egglord0716@gmail.com>
2021-05-17 00:30:15 +0930

    delete function pending

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,51 +1,34 @@
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-
-const dbnotes = require('../db/db.json');
+let dbnotes = require('../db/db.json');
 
 // ROUTING
 
 module.exports = (app) => {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get('/api/notes', (req, res) => res.json(dbnotes));
 
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  app.get('/api/notes/:id', (req, res) => {
+    console.log(dbnotes);
+    console.log(req.params.id);
+    const note = dbnotes.find( n => n.id === parseInt(req.params.id));
+    if (!note) res.status(404).send('This note with the given ID is not found');
+    res.send(note);
+  });
 
   app.post('/api/notes', (req, res) => {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-
-    // if (tableData.length < 5) {
-    //   tableData.push(req.body);
-    //   res.json(true);
-    // } else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
+    // Stop posting this note if it is same as one of the previous notes.
+    for(let i=0; i < dbnotes.length; i++){
+      if(req.body.title == dbnotes[i].title && req.body.text == dbnotes[i].text){ return };
+    }
+    // Add id property to the new note
+    req.body.id = dbnotes.length + 1;  
+    dbnotes.push(req.body);
+    res.json(dbnotes);
   });
 
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
-  app.delet('/api/notes', (req, res) => {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
+  // app.delet('/api/notes', (req, res) => {
+  //   dbnotes.push(req.body);
+  //   res.json(dbnotes);
+  // });
 };
