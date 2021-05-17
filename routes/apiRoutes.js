975ceb577743af61b626024c8c4fbde7925a12e2
ZhouTian0716 <egglord0716@gmail.com>
2021-05-17 21:19:28 +0930

    fs and path modules added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,8 @@
 // LOAD DATA
 let dbnotes = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
+
 
 // ROUTING
 
@@ -8,10 +11,9 @@ module.exports = (app) => {
   app.get('/api/notes', (req, res) => res.json(dbnotes));
 
   app.get('/api/notes/:id', (req, res) => {
-    console.log(dbnotes);
-    console.log(req.params.id);
     const note = dbnotes.find( n => n.id === parseInt(req.params.id));
     if (!note) return res.status(404).send('This note with the given ID is not found');
+    updateDB(dbnotes);
     res.send(note);
   });
 
@@ -23,6 +25,7 @@ module.exports = (app) => {
     // Add id property to the new note
     req.body.id = dbnotes.length;  
     dbnotes.push(req.body);
+    updateDB(dbnotes);
     res.json(dbnotes);
   });
 
@@ -33,6 +36,17 @@ module.exports = (app) => {
     dbnotes.splice(index, 1);
     // To revise note ids after note delete request
     for (let i = index; i < dbnotes.length; i++ ){dbnotes[i].id--}
+    updateDB(dbnotes);
     res.json(dbnotes);
   });
+};
+
+function updateDB(result){
+  fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(result), err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log('SUCCESFULLY WROTE TO THE FILE');
+  })
 };
